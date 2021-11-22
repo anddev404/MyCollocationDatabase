349562fc4d1bff5a76fd4989b906ba05cd80d582
anddev404 <36824866+anddev404@users.noreply.github.com>
2021-11-22 21:53:53 +0100

    Update CollocationDatabase.kt

--- app/myDatabase/src/main/java/com/example/mydatabase/CollocationDatabase.kt
+++ app/myDatabase/src/main/java/com/example/mydatabase/CollocationDatabase.kt
@@ -2,10 +2,16 @@ package com.example.mydatabase
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.mydatabase.dao.CollocationDao
+import com.example.mydatabase.dao.SentenceDao
 import com.example.mydatabase.dao.WordDao
+import com.example.mydatabase.model.Collocation
+import com.example.mydatabase.model.Sentence
 import com.example.mydatabase.model.Word
 
-@Database(entities = [Word::class], version = 1)
+@Database(entities = [Word::class, Collocation::class, Sentence::class], version = 1)
 abstract class CollocationDatabase : RoomDatabase() {
     abstract fun wordDao(): WordDao
+    abstract fun collocationDao(): CollocationDao
+    abstract fun sentenceDao(): SentenceDao
 }
