aa65dbd2f41436ad83526f70f7542f10a270d86b
anddev404 <36824866+anddev404@users.noreply.github.com>
2021-11-22 21:48:12 +0100

    Update Collocation.kt

--- app/myDatabase/src/main/java/com/example/mydatabase/model/Collocation.kt
+++ app/myDatabase/src/main/java/com/example/mydatabase/model/Collocation.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 import com.example.mydatabase.model.Sentence
 
 @Entity(tableName = "Collocation")
-class Collocation(var wordId: Int, var relation: Int, var collocation: String) {
+class Collocation(var wordId: Int, var relation: String, var collocation: String) {
 
     var translation: String? = null
     var isChecked: Boolean = false
@@ -19,16 +19,16 @@ class Collocation(var wordId: Int, var relation: Int, var collocation: String) {
     var id: Int = 0
 
     companion object {
-        val Relation_V_obj_N = "V:obj:N"
-        val Relation_V_prep_N = "V:prep:N"
-        val Relation_V_obj_1_2_N = "V:obj1+2:N"
-        val Relation_V_obj_prep_N = "V:obj+prep:N"
-        val Relation_V_subj_N = "V:subj:N"
-        val Relation_V_sc_V = "V:sc:V"
-        val Relation_N_mod_A = "N:mod:A"
-        val Relation_N_prep_N = "N:prep:N"
-        val Relation_N_nn_N = "N:nn:N"
-        val Relation_V_mod_A = "V:mod:A"
-        val Relation_A_mod_A = "A:mod:A"
+        val RELATION_V_obj_N = "V:obj:N"
+        val RELATION_V_prep_N = "V:prep:N"
+        val RELATION_V_obj_1_2_N = "V:obj1+2:N"
+        val RELATION_V_obj_prep_N = "V:obj+prep:N"
+        val RELATION_V_subj_N = "V:subj:N"
+        val RELATION_V_sc_V = "V:sc:V"
+        val RELATION_N_mod_A = "N:mod:A"
+        val RELATION_N_prep_N = "N:prep:N"
+        val RELATION_N_nn_N = "N:nn:N"
+        val RELATION_V_mod_A = "V:mod:A"
+        val RELATION_A_mod_A = "A:mod:A"
     }
 }
