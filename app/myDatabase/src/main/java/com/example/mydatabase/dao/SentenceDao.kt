e128fa5de45d06b568377a6f0c27b4b7734a0b31
anddev404 <36824866+anddev404@users.noreply.github.com>
2021-11-28 12:02:09 +0100

    Update SentenceDao.kt

--- app/myDatabase/src/main/java/com/example/mydatabase/dao/SentenceDao.kt
+++ app/myDatabase/src/main/java/com/example/mydatabase/dao/SentenceDao.kt
@@ -6,30 +6,14 @@ import com.example.mydatabase.model.Sentence
 @Dao
 interface SentenceDao {
 
-    @Query("SELECT * FROM Sentence")
-    fun getAll(): List<Sentence>
-
-    @Query("SELECT * FROM Sentence WHERE id = :sentenceId")
+    @Query("SELECT * FROM Sentence.sentences WHERE id = :sentenceId")
     fun getById(sentenceId: Int): Sentence?
-
-    @Query("SELECT * FROM Sentence WHERE collocationId = :collocationId")
-    fun getByCollocationId(collocationId: Int): List<Sentence>
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insert(sentence: Sentence): Long
-
 
     @Update
     fun update(sentence: Sentence): Int
 
 
-    @Delete
-    fun delete(sentence: Sentence): Int
-
-    @Query("DELETE FROM Sentence WHERE id = :sentenceId")
-    fun deleteById(sentenceId: Int): Int
-
-    @Query("SELECT COUNT(*) FROM Sentence")
+    @Query("SELECT COUNT(*) FROM Sentence.sentences")
     fun getNumberOfSentences(): Int
 
 }
