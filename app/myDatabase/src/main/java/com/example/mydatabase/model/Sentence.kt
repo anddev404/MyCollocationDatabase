4fd616fa3d2ec5d284c70d512f4170e00bc365ea
anddev404 <36824866+anddev404@users.noreply.github.com>
2021-11-28 12:18:14 +0100

    Update Sentence.kt

--- app/myDatabase/src/main/java/com/example/mydatabase/model/Sentence.kt
+++ app/myDatabase/src/main/java/com/example/mydatabase/model/Sentence.kt
@@ -1,15 +1,51 @@
 package com.example.mydatabase.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "Sentence")
-class Sentence(var collocationId: Int, var sentence: String) {
+@Entity(tableName = "sentences")
+class Sentence(
 
-    var translation: String? = null
-    var isChecked: Boolean = false
-    var numberOfDownloads = 0
+    @ColumnInfo(
+        name = "beispielsatz",
+        defaultValue = ""
+    ) var sentencesAllInOne: String = "",
+
+    @ColumnInfo(defaultValue = "")
+    var translationAllInOne: String = "",
+
+    @ColumnInfo(defaultValue = "0")
+    var isChecked: Boolean = false,
+
+    @ColumnInfo(defaultValue = "0")
+    var numberOfDownloads: Int = 0
+) {
 
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0
+
+
+    @Ignore
+    var sentencesAfterSplitting: ArrayList<String> = arrayListOf()
+
+    @Ignore
+    var translations: ArrayList<String> = arrayListOf()
+
+    fun splitSentences() {
+
+        //TODO rozdzielenie sentencesAllInOne(pobrane z bazy) do sentencesAfterSplitting(do obiektów String). Rozdzielenie wykonane za pomocą znaku splitChar
+
+    }
+
+    fun mergeTranslations() {
+
+        //TODO  połączenie wielu tłumaczeń w celu zapisu do bazy w jednym Stringu, znak rozdzielajacy to splitChar
+
+    }
+
+    companion object {
+        val splitChar = "@!@"
+    }
 }
