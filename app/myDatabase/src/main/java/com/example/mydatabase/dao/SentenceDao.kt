56f692f3e189500503b7897fb13f9aecfa11e06b
anddev404 <36824866+anddev404@users.noreply.github.com>
2021-11-28 12:49:33 +0100

    Update SentenceDao.kt

--- app/myDatabase/src/main/java/com/example/mydatabase/dao/SentenceDao.kt
+++ app/myDatabase/src/main/java/com/example/mydatabase/dao/SentenceDao.kt
@@ -6,11 +6,15 @@ import com.example.mydatabase.model.Sentence
 @Dao
 interface SentenceDao {
 
+    //TODO czy insert wpakuje takze id do obiektu czy tylko go zwraca
+    @Update
+    fun insert(sentence: Sentence): Long
+
     @Query("SELECT * FROM Sentence.sentences WHERE id = :sentenceId")
     fun getById(sentenceId: Int): Sentence?
 
     @Update
-    fun update(sentence: Sentence): Int
+    fun update(sentence: Sentence): Long
 
 
     @Query("SELECT COUNT(*) FROM Sentence.sentences")
