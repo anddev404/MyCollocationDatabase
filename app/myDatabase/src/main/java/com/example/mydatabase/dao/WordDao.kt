75f8ed9e154141f9d0ace7f5c0add96082fbe6d3
anddev404 <36824866+anddev404@users.noreply.github.com>
2021-11-28 12:21:20 +0100

    Update WordDao.kt

--- app/myDatabase/src/main/java/com/example/mydatabase/dao/WordDao.kt
+++ app/myDatabase/src/main/java/com/example/mydatabase/dao/WordDao.kt
@@ -6,29 +6,30 @@ import com.example.mydatabase.model.Word
 @Dao
 interface WordDao {
 
-    @Query("SELECT * FROM Word")
+    @Query("SELECT * FROM Word.words")
     fun getAll(): List<Word>
 
-    @Query("SELECT * FROM Word WHERE id = :userIds")
+    @Query("SELECT * FROM Word.words WHERE id = :userIds")
     fun getById(userIds: Int): Word?
 
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insert(word: Word): Long
 
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(words: List<Word>): Int
 
     @Update
     fun update(word: Word): Int
-
-
+    
     @Delete
     fun delete(word: Word): Int
 
-    @Query("DELETE FROM Word WHERE id = :userIds")
+    @Query("DELETE FROM Word.words WHERE id = :userIds")
     fun deleteById(userIds: Int): Int
 
 
-    @Query("SELECT COUNT(*) FROM Word")
+    @Query("SELECT COUNT(*) FROM Word.words")
     fun getNumberOfWords(): Int
 
 }
