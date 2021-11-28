993eea7c39b9da94b1975f66409e6634ea6e2f16
anddev404 <36824866+anddev404@users.noreply.github.com>
2021-11-28 13:36:31 +0100

    Update SentenceDao.kt

--- app/myDatabase/src/main/java/com/example/mydatabase/dao/SentenceDao.kt
+++ app/myDatabase/src/main/java/com/example/mydatabase/dao/SentenceDao.kt
@@ -10,14 +10,14 @@ interface SentenceDao {
     @Insert
     fun insert(sentence: Sentence): Long
 
-    @Query("SELECT * FROM Sentence.sentences WHERE id = :sentenceId")
+    @Query("SELECT * FROM sentences WHERE id = :sentenceId")
     fun getById(sentenceId: Int): Sentence?
 
     @Update
-    fun update(sentence: Sentence): Long
+    fun update(sentence: Sentence): Int
 
 
-    @Query("SELECT COUNT(*) FROM Sentence.sentences")
+    @Query("SELECT COUNT(*) FROM sentences")
     fun getNumberOfSentences(): Int
 
 }
