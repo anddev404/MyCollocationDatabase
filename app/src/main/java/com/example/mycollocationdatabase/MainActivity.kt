61033159f415da743bbf8cea7953769d93895fe5
anddev404 <36824866+anddev404@users.noreply.github.com>
2021-12-09 21:55:37 +0100

    test database

--- app/src/main/java/com/example/mycollocationdatabase/MainActivity.kt
+++ app/src/main/java/com/example/mycollocationdatabase/MainActivity.kt
@@ -1,15 +1,28 @@
 package com.example.mycollocationdatabase
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.room.Room.databaseBuilder
 import com.example.mydatabase.CollocationDatabase
+import com.example.mydatabase.dao.CollocationDao
 import com.example.mydatabase.dao.SentenceDao
+import com.example.mydatabase.dao.WordDao
+import com.example.mydatabase.model.Collocation
+import com.example.mydatabase.model.Sentence
+import com.example.mydatabase.model.Word
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var sentenceDao: SentenceDao
+    lateinit var wordDao: WordDao
+    lateinit var collocationDao: CollocationDao
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +30,30 @@ class MainActivity : AppCompatActivity() {
 
         var context = this
         val db = initializeDatabase()
-        sentenceDao = db.sentenceDao()
 
+        collocationDao = db.collocationDao()
+        sentenceDao = db.sentenceDao()
+        wordDao = db.wordDao()
+        
+        GlobalScope.launch {
+
+            collocationDao.insert(Collocation("Test", "Translation", "relation", Random.nextInt()))
+            sentenceDao.insert(Sentence("Hey"))
+            wordDao.insert(Word("Home", "Hołm", Random.nextInt()))
+
+            var c = collocationDao.getNumberOfCollocations()
+            var s = sentenceDao.getNumberOfSentences()
+            var w = wordDao.getNumberOfWords()
+
+            Handler(Looper.getMainLooper()).postDelayed(
+                {
+                    Toast.makeText(context, "=== $c, $s, $w ===", Toast.LENGTH_LONG).show();
+                },
+                0
+            )
+
+
+        }
     }
 
 
